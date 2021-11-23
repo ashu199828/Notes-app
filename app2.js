2f1c5367554c79be9b5a0fc441c1fcfe4af497ee
ashu199828 <93378896+ashu199828@users.noreply.github.com>
2021-11-23 13:23:12 +0530

    first commit

---
+++ app2.js
@@ -0,0 +1,64 @@
+showNotes();
+addBtn = document.getElementById('addBtn')
+addBtn = document.getElementById('addBtn')
+addBtn.addEventListener('click',function(e){
+    addTxt = document.getElementById('addTxt')
+    notes = localStorage.getItem('notes')
+    if(notes ==null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes)
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj)
+    showNotes();
+})
+function showNotes(){
+    notes = localStorage.getItem('notes')
+    if(notes ==null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes)
+    }
+    html = ''
+    notesObj.forEach(function(element, index) {
+        html +=`
+        <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Note${index + 1}</h5>
+          <p class="card-text">${element}</p>
+          <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+        </div>
+      </div> `;
+        
+    });
+
+    notesElm = document.getElementById('notes')
+    if(notesObj.length !=0){
+        html
+        
+
+    }
+    else{
+        notesElm.innerHTML =`Nothing to show! Use "Add a Note" section above to add notes.`
+
+    }
+}
+function deleteNote(index){
+    notes = localStorage.getItem('notes')
+    if(notes ==null){
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes)
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem('notes', JSON.stringify(notesObj));
+    showNotes();
+
+
+}
